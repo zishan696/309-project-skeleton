15d193116902e636732990fcee34edc32bc3911f
Shama Hoque <shama.hoque@gmail.com>
2017-03-19 19:59:58 +0600

    product routes

---
+++ routes/product.server.routes.js
@@ -0,0 +1,20 @@
+module.exports = function(app){
+
+ var products = require('./../controllers/products.server.controller.js');
+ var users = require('./../controllers/users.server.controller.js');
+
+ app.route('/api/products')
+	.get(products.list)
+	.post(users.requiresLogin, products.create);
+
+  app.route('/api/products/:productId')
+	.get(products.read)
+  .delete(users.requiresLogin, products.delete);
+
+	app.route('/api/products/edit/:productId')
+	.get(products.read)
+	.put(users.requiresLogin, products.update);
+
+  app.param('productId', products.productByID);
+
+}
